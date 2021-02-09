3085a7124f983ae8e19bb549ec7006a90997fc2a
fundante2050 <fundante2050@gmail.com>
2021-02-08 19:09:28 -0600

    Agregar variables

--- src/EjemploGit.java
+++ src/EjemploGit.java
@@ -4,6 +4,8 @@ public class EjemploGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola compañeros de Git");
+		String nombre = "Juan Diaz";
+		
 
 	}
 
